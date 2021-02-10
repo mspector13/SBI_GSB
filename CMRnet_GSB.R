dfb3850c374a9de3df7b56b2405a0f7a1a4e2469
mspector13 <mspector.ecology@gmail.com>
2021-02-09 16:44:34 -0800

    Saved file, got everything back in working order after accidental deletions. Should be able to move forward now

--- CMRnet_GSB.R
+++ CMRnet_GSB.R
@@ -35,6 +35,7 @@ write_csv(IntrData, file = "Trans_ID.csv") #save to repo and edit in Excel lmao
 
 #Load the data ####
 Trans_ID <- read_csv("Trans_ID.csv", na="NULL")
+#Remember to remove the "A69-1602-" manually in Excel, until a better script is written
 
 #From the "coordmeters.R" script to translate lat/longs into a grid
 #LOAD AND RUN "coordmeters.R" first!
@@ -45,14 +46,21 @@ Trans_ID <- Trans_ID %>%
   rename(y=y1)
 
 #Then use lubridate to get the character string into the correct format 
-Trans_ID$date <- mdy_hms(Trans_ID$date) #note: syntax is for original format *not* desired
+#Trans_ID$date <- mdy_hms(Trans_ID$date) #note: syntax is for original format *not* desired
 
 New_data <- Trans_ID %>% 
-  mutate(id = as.integer(id)) %>% 
-  mutate(loc = as.factor(loc)) %>% 
-  mutate(x = as.integer(x)) %>% 
-  mutate(y = as.integer(y))
-New_data
+  dplyr::mutate(id = as.integer(id)) %>% 
+  dplyr::mutate(loc = as.factor(loc)) %>% 
+  dplyr::mutate(x = as.integer(x)) %>% 
+  dplyr::mutate(y = as.integer(y))
+
+#This should, ideally, group by unique DAYS (not minutes or seconds w/in days)
+New_data %>% 
+  mutate(date = date(date)) %>% 
+  group_by(id, loc, x, y, date) %>% 
+  summarize(n_detections = n()) %>% 
+  arrange(desc(n_detections))
+  
 
 #Construct co-capture networks ####
 mindate <- "2018-08-13"
